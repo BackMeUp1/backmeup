5e9eac5cc952c7c1d83fbfff5cfd637bb30d9c92
mohamed501234 <hama.bennacef50@gmail.com>
2023-09-01 14:34:22 +0100

    fix admin

--- frontend/src/components/Added.jsx
+++ frontend/src/components/Added.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Swal from 'sweetalert2';
 import axios from "axios"
-import './ModalComponent.css'; 
+// import './ModalComponent.css'; 
 import { useNavigate } from 'react-router';
 const ModalComponent = (props) => {
   const [selectedCategory, setSelectedCategory] = useState('');
--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './AdminDashboard.css';
-import AllProject from './AllProjects.jsx';
-import Demande from "./Demande"
-import UserList from './userList';
+import Cookies from 'js-cookie';
 
+const AdminDashboard = () => {
+  const handleLogout = () => {
+    Cookies.remove('token'); 
+    };
 
-const AdminDashboard = (props) => {
-  const {projects} =props
   return (
     <div className="admin-dashboard">
       <div className="sidebar">
@@ -17,37 +17,15 @@ const AdminDashboard = (props) => {
             <Link to="/admin/All-project">All Project</Link>
           </li>
           <li>
-            <Link to="/admin/users">User </Link>
+            <Link to="/admin/users">User</Link>
           </li>
           <li>
-            <Link to="/admin/Demande">Demande</Link>
+            <button onClick={handleLogout}>Logout</button>
           </li>
         </ul>
       </div>
-      <div className="content">
-        <Routes>
-          <Route path="/admin/All-project" element={<AllProject projects/>} />
-          <Route path="/admin/projects" element={<ProjectList />} />
-          <Route path="/admin/list" element={<Demande />} />
-          <Route path="/admin/users" element={<UserList />} />
-        </Routes>
-      </div>
     </div>
   );
 };
-
-
-const ProjectList = () => {
-  // Implement the component logic for displaying the list of projects
-  // This component will fetch the project data from the API and render it
-  return (
-    <div>
-      <h2>Projects</h2>
-      <p>List of projects goes here.</p>
-    </div>
-  );
-};
-
-
 
 export default AdminDashboard;
